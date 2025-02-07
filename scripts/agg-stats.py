47632608df155add59e0dd986c04cbb9791d20c8
Cameron Pittman <cameron.w.pittman@gmail.com>
2025-02-07 14:29:37 -0500

    collect team records per season

--- scripts/agg-stats.py
+++ scripts/agg-stats.py
@@ -3,6 +3,7 @@ import json
 import pandas as pd
 from pathlib import Path
 import shutil
+from typing import List, TypedDict
 
 # TODO keep an old json around per season. lets us show last season's stats at the beginning of next season
 
@@ -10,10 +11,10 @@ import shutil
 xbl_season_18.json
 {
     current_season: 18,
-    season_standings: [team1, team2, ...],
     season_team_records: [
         {
             team,
+            rank,
             wins,
             losses,
             remaining,
@@ -224,6 +225,7 @@ LEAGUES = ["XBL", "AAA", "AA"]
 
 class MyNamespace(argparse.Namespace):
     season: int
+    json_dir: Path
 
 
 def arg_parser():
@@ -231,16 +233,35 @@ def arg_parser():
         description="Aggregate XBL stats per-season and for careers"
     )
     parser.add_argument("-s", "--season", type=int, help="Current season")
+    parser.add_argument(
+        "-j",
+        "--json-dir",
+        type=Path,
+        default=Path("public/json"),
+        help="Path to where JSON is stored",
+    )
 
     return parser
 
 
-def build_season_stats(league: str, season: int):
+class TeamRecord(TypedDict):
+    team: str
+    rank: int
+    wins: int
+    losses: int
+    remaining: int
+
+
+class SeasonStats(TypedDict):
+    current_season: int
+    season_team_records: List[TeamRecord]
+
+
+def build_season_stats(league: str, json_dir: Path, season: int) -> SeasonStats:
     tabs = ["Standings", "Hitting", "Pitching", "Playoffs", "Box%20Scores"]
 
     data = {
         "current_season": season,
-        "season_standings": [],
         "season_team_records": [],
         "season_team_stats": [],
         "season_game_results": [],
@@ -249,30 +270,62 @@ def build_season_stats(league: str, season: int):
         "playoffs_game_results": [],
     }
 
-    # TODO do parsing
+    standings_data = None
+    with open(json_dir.joinpath(f"{league}__Standings.json")) as f:
+        raw_data = json.loads(f.read())
+        standings_data = raw_data["values"]
+
+    win_col = 4 if league == "AA" else 2
+    loss_col = 5 if league == "AA" else 3
+
+    data["season_team_records"] = [
+        {
+            "team": value[1],
+            "rank": int(value[0]),
+            "wins": int(value[win_col]),
+            "losses": int(value[loss_col]),
+            "remaining": None,
+        }
+        # first row is the header
+        for value in standings_data[1:]
+    ]
+
+    team_count = len(data["season_team_records"])
+    games_per_team = 2 * (team_count - 1)
+
+    for i in range(len(data["season_team_records"])):
+        games_played = (
+            data["season_team_records"][i]["wins"]
+            + data["season_team_records"][i]["losses"]
+        )
+        # I think Ws and Ls in the spreadsheet don't account for teams who drop?
+        data["season_team_records"][i]["remaining"] = max(
+            games_per_team - games_played, 0
+        )
 
     return data
 
 
 def main(args: MyNamespace):
-    json_dir = Path("public/json")
-    if not json_dir.exists():
+    if not args.json_dir.exists():
         raise Exception("Missing data. Plesae run `scripts/get-sheets.py' first")
 
     # data = readjson()
     # df = pd.DataFrame(dict(zip(data[0], data[1:])))
 
     season_data = {
-        league: build_season_stats(league, args.season) for league in LEAGUES
+        league: build_season_stats(league, args.json_dir, args.season)
+        for league in LEAGUES
     }
 
     # write json data
     for league in LEAGUES:
-        season_json = f"{league}__s{args.season}.json"
-        with open(season_json, "w") as f:
-            f.write(json.dumps(season_data[league]))
+        print(season_data[league])
+        # season_json = args.json_dir.joinpath(f"{league}__s{args.season}.json")
+        # with open(season_json, "w") as f:
+        #     f.write(json.dumps(season_data[league]))
 
-        shutil.copy(season_json, f"{league}.json")
+        # shutil.copy(season_json, args.json_dir.joinpath(f"{league}.json"))
 
 
 if __name__ == "__main__":
