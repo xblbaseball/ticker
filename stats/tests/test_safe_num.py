180e5caf347de406daef9c469af2bfa5fc4c8924
Cameron Pittman <cameron.w.pittman@gmail.com>
2025-02-14 10:46:11 -0500

    improve compatibility of SafeNum

--- stats/tests/test_safe_num.py
+++ stats/tests/test_safe_num.py
@@ -1,6 +1,8 @@
+import json
+import math
 import unittest
 
-from stats.utils import SafeNum
+from stats.utils import SafeEncoder, SafeNum
 
 
 class TestSafeNumOperations(unittest.TestCase):
@@ -117,29 +119,169 @@ class TestSafeNumOperations(unittest.TestCase):
 
         self.assertEqual(y, 6, "*= between a SafeNum and an int")
 
+    def test_dividing_float(self):
+        x = SafeNum(3)
+        y = SafeNum(2)
+
+        self.assertEqual(x / y, 1.5, "Two SafeNums")
+
+        x = 1
+        y = SafeNum(2)
+
+        self.assertEqual(x / y, 0.5, "A SafeNum and an int")
+
+        x = 1.0
+        y = SafeNum(2)
+
+        self.assertEqual(x / y, 0.5, "A SafeNum and a float")
+
+        x = SafeNum(3)
+        y = SafeNum(2)
+
+        x /= y
+
+        self.assertEqual(x, 1.5, "/= between SafeNums")
+
+        x = SafeNum(3)
+        y = 2
+
+        x /= y
+
+        self.assertEqual(x, 1.5, "/= between an int and SafeNum")
+
+        x = SafeNum(4)
+        y = 2
+
+        y /= x
+
+        self.assertEqual(y, 0.5, "/= between a SafeNum and an int")
+
+        x = SafeNum(4)
+        y = 0
+
+        self.assertEqual(x / y, None, "x / 0 = None")
+
+    def test_dividing_int(self):
+        x = SafeNum(3)
+        y = SafeNum(2)
+
+        self.assertEqual(x // y, 1, "Two SafeNums")
+
+        x = 5
+        y = SafeNum(2)
+
+        self.assertEqual(x // y, 2, "A SafeNum and an int")
+
+        x = 11.0
+        y = SafeNum(2)
+
+        self.assertEqual(x // y, 5, "A SafeNum and a float")
+
+        x = SafeNum(3)
+        y = SafeNum(2)
+
+        x //= y
+
+        self.assertEqual(x, 1, "//= between SafeNums")
+
+        x = SafeNum(3)
+        y = 2
+
+        x //= y
+
+        self.assertEqual(x, 1, "//= between an int and SafeNum")
+
+        x = SafeNum(4)
+        y = 2
+
+        y //= x
+
+        self.assertEqual(y, 0, "//= between a SafeNum and an int")
+
+        x = SafeNum(4)
+        y = 0
+
+        self.assertEqual(x // y, None, "x / 0 = None")
+
     def test_nones(self):
         x = SafeNum(3)
 
-        self.assertIsNone(x + None, "SafeNum + None = None")
-        self.assertIsNone(x - None, "SafeNum - None = None")
-        self.assertIsNone(x * None, "SafeNum * None = None")
-        self.assertIsNone(x / None, "SafeNum / None = None")
-        self.assertIsNone(None + x, "None + SafeNum = None")
-        self.assertIsNone(None - x, "None - SafeNum = None")
-        self.assertIsNone(None * x, "None * SafeNum = None")
-        self.assertIsNone(None / x, "None / SafeNum = None")
+        self.assertEqual(x + None, None, "SafeNum + None = None")
+        self.assertEqual(x - None, None, "SafeNum - None = None")
+        self.assertEqual(x * None, None, "SafeNum * None = None")
+        self.assertEqual(x / None, None, "SafeNum / None = None")
+        self.assertEqual(None + x, None, "None + SafeNum = None")
+        self.assertEqual(None - x, None, "None - SafeNum = None")
+        self.assertEqual(None * x, None, "None * SafeNum = None")
+        self.assertEqual(None / x, None, "None / SafeNum = None")
 
         a = SafeNum(3)
         a += None
 
-        self.assertIsNone(a, "SafeNum += None")
+        self.assertEqual(a, None, "SafeNum += None")
 
         a = SafeNum(3)
         a -= None
 
-        self.assertIsNone(a, "SafeNum -= None")
+        self.assertEqual(a, None, "SafeNum -= None")
 
         a = SafeNum(3)
         a *= None
 
-        self.assertIsNone(a, "SafeNum *= None")
+        self.assertEqual(a, None, "SafeNum *= None")
+
+        a = SafeNum(3)
+        a /= None
+
+        self.assertEqual(a, None, "SafeNum /= None")
+
+    def test_series_of_operations(self):
+        a = SafeNum(3)
+        b = SafeNum(4)
+        c = SafeNum(5)
+
+        x = math.sqrt(a**2 + b**2 + c**2)
+
+        self.assertEqual(x, math.sqrt(50), "3d euclidean distance")
+
+        x = a + b / 10
+
+        self.assertEqual(x, 3.4, "add and divide")
+
+        d = SafeNum(0)
+
+        y = (a + b + c / d) / 4
+
+        self.assertEqual(y, None, "dividing by 0 is None and it turns everything None")
+
+        y = (a + 4 + c / None) / 5
+
+        self.assertEqual(y, None, "dividing by None is None")
+
+    def test_json_dump(self):
+        a = SafeNum(4)
+
+        dumped = json.dumps({"d": a}, cls=SafeEncoder)
+
+        self.assertEqual(dumped, '{"d": 4}', "int JSON stringified")
+
+        a = SafeNum(4.0)
+
+        dumped = json.dumps({"d": a}, cls=SafeEncoder)
+
+        self.assertEqual(dumped, '{"d": 4.0}', "float JSON stringified")
+
+        dumped_with_none = json.dumps({"d": SafeNum(None)}, cls=SafeEncoder)
+
+        self.assertEqual(dumped_with_none, '{"d": null}', "None JSON stringified")
+
+    def test_is_none(self):
+        a = SafeNum(0)
+
+        self.assertFalse(a.is_none, "0 is not None")
+
+        b = SafeNum(None)
+
+        self.assertTrue(b.is_none, "None is None")
+
+        self.assertIsNotNone(b, "SafeNum cannot be used with `is None' syntax")
--- stats/utils/safe_num.py
+++ stats/utils/safe_num.py
@@ -1,53 +1,72 @@
-class SafeNum:
-    """Implements a number that has a contagious None. Many columns are missing data. If data from a column is missing, we can't do a calculation accurately. This allows a number to turn into None without freaking out. But any further calculations with None always return None. Also dividing by 0 turns into None. Otherwise, this should behave like a normal number"""
+from json import JSONEncoder
+import numbers
+
+
+class SafeNum(numbers.Number):
+    """
+    Implements a number that has a contagious None. Many columns are missing data. If data from a column is missing, we can't do a calculation accurately. This allows a number to turn into None without freaking out. But any further calculations with None always return SafeNum(None. Also dividing by 0 turns into None. Otherwise, this should behave like a normal )number
+
+    The reason we use None instead of something like NaN or Infinity is because None is JSON serializable, while other non-numeric values are not.
+
+    FYI, this class is not compatible with `is None' syntax
+    """
 
     def __init__(self, x: float | int | str | None):
         if x is None:
-            self.x = None
+            self._x = None
         elif isinstance(x, str):
             try:
                 y = float(x)
-                self.x = y
+                self._x = y
             except ValueError as e:
-                self.x = None
+                self._x = None
         else:
-            self.x = x
+            self._x = x
+
+    def __int__(self):
+        if self._x is None:
+            return int("nan")
+
+        return int(self._x)
+
+    def __float__(self):
+        if self._x is None:
+            return float("nan")
+
+        return float(self._x)
 
     def __repr__(self):
         """string representation"""
-        return str(self.x)
+        return str(self._x)
 
     def __eq__(self, other):
         """=="""
         if isinstance(other, SafeNum):
-            return self.x == other.x
+            return self._x == other._x
 
-        if self.x is None or other is None:
-            return False
-
-        return self.x == other
+        return self._x == other
 
     def __ne__(self, other):
         """!="""
-        return not self.x == other
+        return not self._x == other
 
     def __neg__(self):
         """negative"""
-        return -1 * self.x
+        return -1 * self._x
 
     def __add__(self, other):
         """+"""
-        if self.x is None or other is None:
-            return None
+        if self._x is None or other is None:
+            return SafeNum(None)
 
-        return SafeNum(self.x + other)
+        return SafeNum(self._x + other)
 
     def __iadd__(self, other):
         """+="""
         if other is None:
-            return None
+            return SafeNum(None)
 
-        self.x = self.x + other
+        self._x = self._x + other
         return self
 
     def __radd__(self, other):
@@ -56,39 +75,39 @@ class SafeNum:
 
     def __sub__(self, other):
         """-"""
-        if self.x is None or other is None:
-            return None
+        if self._x is None or other is None:
+            return SafeNum(None)
 
         return self.__add__(-other)
 
     def __isub__(self, other):
         """-="""
         if other is None:
-            return None
+            return SafeNum(None)
 
-        self.x = self.x - other
+        self._x = self._x - other
         return self
 
     def __rsub__(self, other):
         """right side of -"""
         if other is None:
-            return None
+            return SafeNum(None)
 
-        return other - self.x
+        return other - self._x
 
     def __mul__(self, other):
         """*"""
-        if self.x is None or other is None:
-            return None
+        if self._x is None or other is None:
+            return SafeNum(None)
 
-        return SafeNum(self.x * other)
+        return SafeNum(self._x * other)
 
     def __imul__(self, other):
         """*="""
         if other is None:
-            return None
+            return SafeNum(None)
 
-        self.x = self.x * other
+        self._x = self._x * other
         return self
 
     def __rmul__(self, other):
@@ -98,21 +117,95 @@ class SafeNum:
     def __floordiv__(self, other):
         """// returns an int"""
         if other == 0 or other is None:
-            return None
+            return SafeNum(None)
 
-        return SafeNum(self.x * (1 // other))
+        return SafeNum(self._x // other)
+
+    def __ifloordiv__(self, other):
+        """//= returns an int"""
+        if other is None:
+            return SafeNum(None)
+
+        self._x = self._x // other
+        return self
 
     def __rfloordiv__(self, other):
         """// returns an int"""
-        return self.__floordiv__(other)
+        if self is None or other is None or self._x == 0:
+            return SafeNum(None)
+
+        return SafeNum(other // self._x)
 
     def __truediv__(self, other):
         """/ returns a float"""
-        if other == 0 or other is None:
-            return None
+        if self._x is None or other is None or other == 0:
+            return SafeNum(None)
 
-        return SafeNum(self.x * (1 / other))
+        return SafeNum(self._x / other)
+
+    def __itruediv__(self, other):
+        """/= returns a float"""
+        if self._x is None or other is None:
+            return SafeNum(None)
+
+        self._x = self._x / other
+        return self
 
     def __rtruediv__(self, other):
         """/ returns a float"""
-        return self.__truediv__(other)
+        if self._x is None or other is None or self._x == 0:
+            return SafeNum(None)
+
+        return SafeNum(other / self._x)
+
+    def __pow__(self, other):
+        """**"""
+        if other is None:
+            return SafeNum(None)
+
+        return SafeNum(self._x**other)
+
+    def __ipow__(self, other):
+        """**="""
+        if other is None:
+            return SafeNum(None)
+
+        self._x = self._x**other
+        return self
+
+    @property
+    def is_none(self):
+        """is the value held None"""
+        return self._x is None
+
+
+class SafeEncoder(JSONEncoder):
+    """JSON encoder that can handle SafeNum, otherwise it's normal"""
+
+    def __init__(
+        self,
+        *,
+        skipkeys=False,
+        ensure_ascii=True,
+        check_circular=True,
+        allow_nan=True,
+        sort_keys=False,
+        indent=None,
+        separators=None,
+        default=None
+    ):
+        super().__init__(
+            skipkeys=skipkeys,
+            ensure_ascii=ensure_ascii,
+            check_circular=check_circular,
+            allow_nan=allow_nan,
+            sort_keys=sort_keys,
+            indent=indent,
+            separators=separators,
+            default=default,
+        )
+
+    def default(self, obj):
+        if isinstance(obj, SafeNum):
+            return obj._x
+        return super().default(obj)
