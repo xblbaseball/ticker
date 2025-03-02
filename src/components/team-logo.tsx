22412b3d689baf837aa9ff09466af1aeee32146a
Cameron Pittman <cameron.w.pittman@gmail.com>
2025-03-02 13:18:39 -0500

    fix handling logos with punctuation in them

--- src/components/team-logo.tsx
+++ src/components/team-logo.tsx
@@ -6,8 +6,11 @@ export default function TeamLogo(
   { team, width = "72px", small = false }:
     { team: string, width?: string, small?: boolean }
 ) {
+  // no punctuation
+  const cleanedName = team.replaceAll(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+
   return <>
-    <img src={`${basePath}/logos/${team}${small ? "-72x72" : ""}.png`}
+    <img src={`${basePath}/logos/${cleanedName}${small ? "-72x72" : ""}.png`}
       alt={`${team} logo`}
       style={{ width, height: "auto" }}
     />
