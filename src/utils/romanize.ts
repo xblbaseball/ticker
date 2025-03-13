4fed94f67e422ade0418b28b7a78f93d6cc5675a
spacemanspumoni <sincerelyxyz414@gmail.com>
2025-03-13 14:16:24 -0500

    romanize the seasons on the sidebar (#14)
    * romanize the seasons on the sidebar
    
    * drop font size for season to 26px + default toRoman export
    
    * regular text in stats section

---
+++ src/utils/romanize.ts
@@ -0,0 +1,32 @@
+export default function toRoman(value: number) {
+    if (isNaN(value)) {
+      return '';
+    }
+  
+    const romanNumerals = [
+      { value: 1000, numeral: 'M' },
+      { value: 900, numeral: 'CM' },
+      { value: 500, numeral: 'D' },
+      { value: 400, numeral: 'CD' },
+      { value: 100, numeral: 'C' },
+      { value: 90, numeral: 'XC' },
+      { value: 50, numeral: 'L' },
+      { value: 40, numeral: 'XL' },
+      { value: 10, numeral: 'X' },
+      { value: 9, numeral: 'IX' },
+      { value: 5, numeral: 'V' },
+      { value: 4, numeral: 'IV' },
+      { value: 1, numeral: 'I' }
+    ];
+  
+    let romanNumeral = '';
+  
+    for (let i = 0; i < romanNumerals.length; i++) {
+      while (value >= romanNumerals[i].value) {
+        romanNumeral += romanNumerals[i].numeral;
+        value -= romanNumerals[i].value;
+      }
+    }
+  
+    return romanNumeral;
+  }
