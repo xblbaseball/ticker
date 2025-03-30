a55f081e26c48286a5e0dfd469ccd2aee19d3086
Cameron Pittman <cameron.w.pittman@gmail.com>
2025-03-30 12:21:27 -0400

    add team logo fallback to league logo

--- src/components/team-logo.tsx
+++ src/components/team-logo.tsx
@@ -1,12 +1,24 @@
 /* eslint-disable @next/next/no-img-element */
 
+import { League } from "@/typings/league";
+
 const basePath = process.env.NEXT_PUBLIC_BASEPATH || "";
 
 const punctuation = /[.,\/#!$%\^&\*;:{}=\-_`~()]/g;
 
+/** render a team's logo */
 export default function TeamLogo(
-  { team, width = "72px", small = false }:
-    { team: string, width?: string, small?: boolean }
+  { team, width = "72px", small = false, league = "XBL" }:
+    {
+      /** team name with proper caps and spaces. punctuation doesn't matter */
+      team: string,
+      /** width for img */
+      width?: string,
+      /** 72x72 version */
+      small?: boolean,
+      /** used to determine fallback logo if the team is missing */
+      league?: League
+    }
 ) {
   // no punctuation
   const cleanedName = team.replaceAll(punctuation, "");
@@ -15,6 +27,10 @@ export default function TeamLogo(
     <img src={`${basePath}/logos/${cleanedName}${small ? "-72x72" : ""}.png`}
       alt={`${team} logo`}
       style={{ width, height: "auto" }}
+      onError={({ currentTarget }) => {
+        currentTarget.onerror = null; // prevents looping
+        currentTarget.src = `${basePath}/logos/${league}-72x72.png`;
+      }}
     />
   </>
 }
