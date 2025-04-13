5781c5f8e83c5357d65da6f76501d1570258cd4e
Cameron Pittman <cameron.w.pittman@gmail.com>
2025-04-13 12:29:15 -0400

    no flexibility in viewport size - always 1920x1080

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="viewport" content="width=1920, height=1080, initial-scale=1, user-scalable=no" />
+      </Head>
       <body>
         <Main />
         <NextScript />
