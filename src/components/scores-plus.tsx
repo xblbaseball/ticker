80f50d6f879757c7d6bbbcf5e7a4633c737a8819
Cameron Pittman <cameron.w.pittman@gmail.com>
2025-04-13 12:42:04 -0400

    properly render logos in score box

--- src/components/scores-plus.tsx
+++ src/components/scores-plus.tsx
@@ -2,6 +2,7 @@
 
 import _ from "lodash";
 import { useContext, useState } from "react";
+import TeamLogo from "@/components/team-logo";
 import { SettingsContext } from "@/store/settings.context";
 import { StatsContext } from "@/store/stats.context";
 import useInterval from "@/utils/useInterval";
@@ -16,8 +17,6 @@ import {
 
 import styles from "./scores-plus.module.css";
 
-const basePath = process.env.NEXT_PUBLIC_BASEPATH || "";
-
 function Team(
   { teamName, score, winner }:
     { teamName: string, score: number, winner: boolean }) {
@@ -30,9 +29,7 @@ function Team(
   return <div className={classes.join(" ")}>
     <div className="flex flex-start">
       <div className={styles.teamLogo}>
-        <img src={`${basePath}/logos/${teamName}.png`}
-          alt={`${teamName} logo`}
-          style={{ maxWidth: "28px" }} />
+        <TeamLogo team={teamName} width="28px" small={true} />
       </div>
       <div className={styles.teamName}>{teamName}</div>
     </div>
